7b8e6d71ca103d36e6b9bf2010d2c3ac85f9db82
hardi-mht <mehtahardi123@gmail.com>
2021-01-04 23:04:44 +0530

    DB added

---
+++ PM_Assignment/Controllers/DataConnectController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PM_Assignment;
+
+namespace PM_Assignment.Controllers
+{
+    public class DataConnectController : Controller
+    {
+        private ProductManagementEntities db = new ProductManagementEntities();
+
+        // GET: DataConnect
+        public ActionResult Index()
+        {
+            return View(db.user_master.ToList());
+        }
+
+        // GET: DataConnect/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_master user_master = db.user_master.Find(id);
+            if (user_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_master);
+        }
+
+        // GET: DataConnect/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DataConnect/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,EmailID,Password")] user_master user_master)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user_master.Add(user_master);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user_master);
+        }
+
+        // GET: DataConnect/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_master user_master = db.user_master.Find(id);
+            if (user_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_master);
+        }
+
+        // POST: DataConnect/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,EmailID,Password")] user_master user_master)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_master).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_master);
+        }
+
+        // GET: DataConnect/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_master user_master = db.user_master.Find(id);
+            if (user_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_master);
+        }
+
+        // POST: DataConnect/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user_master user_master = db.user_master.Find(id);
+            db.user_master.Remove(user_master);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
